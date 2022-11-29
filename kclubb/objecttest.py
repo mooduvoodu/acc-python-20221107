cf1de2b669b371313e2377e99ee99cce1284a298
mooduvoodu <kclubb@austincc.edu>
2022-11-29 02:30:53 +0000

    new code

---
+++ kclubb/objecttest.py
@@ -0,0 +1,17 @@
+var1 = 'kyle'
+var2 = 'kyle' + 'kyle'
+
+import pandas
+import requests
+
+x = requests.get('http://www.yahoo.com')
+print(x.text)
+
+
+class myname:
+    def func1():
+        pass
+    def funct2():
+        pass
+    insidemember = 1
+
--- kclubb/test.py
+++ kclubb/test.py
@@ -4,4 +4,7 @@ import pandas as pd
 
 
 df = pd.read_csv('classfiles/gapminder/gapminder.tsv', sep='\t')
+
 print(df)
+
+

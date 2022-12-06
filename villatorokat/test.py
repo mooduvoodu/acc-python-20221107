26c5c752cd1deaee52539b021e483596d0fe22de
villakat <117993828+villakat@users.noreply.github.com>
2022-12-06 01:44:47 +0000

    columns, rows

--- villatorokat/test.py
+++ villatorokat/test.py
@@ -4,4 +4,13 @@ df=pd.read_csv('classfiles/gapminder/gapminder.tsv', sep='\t')
 pd.read_csv
 
 
-print(df)
+#print(df)
+
+
+#print(df.info())
+
+coltosee=df[['country','lifeExp']]
+#print(coltosee.head(10))
+#print(coltosee.tail(10))
+
+print(df.loc[155:162])

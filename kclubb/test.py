5f443d0a0ab023b25cda4e58f5aa639f2538831b
mooduvoodu <kclubb@austincc.edu>
2022-12-06 00:41:29 +0000

    more files

--- kclubb/test.py
+++ kclubb/test.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
 
+
 df = pd.read_csv('classfiles/gapminder/gapminder.tsv', sep='\t')
+pd.read_csv
 
 print(df)
+print(df.shape)
+
+
+
+print(df.loc[0:9,['country','year']])
+
+
+
 
 
